7688d9cc1fe305a7f52173b5184fa35d7a1dadcc
Michela Bicocchi <mbicocchi1@gmail.com>
2023-03-30 14:52:06 +0100

    update event handler to add image to boxes in turns

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const newGameBtn = document.querySelector('.button button');
 const gameGrid = document.querySelector('.grid');
+let isX = true;
 
 newGameBtn.addEventListener('click', startNewGame);
 
 gameGrid.addEventListener('click', function(e) {
   const box = e.target
   const image = document.createElement('img')
-  image.src = "images/x1.svg"
+  if (isX) {
+    image.src = "images/x1.svg"
+  } else {
+    image.src = "images/o1.svg"
+  }
+  isX = !isX;
   box.appendChild(image)
 });
 
