c5c8167eca18dceb243cf681e838d2c7eacb75b2
Michela Bicocchi <mbicocchi1@gmail.com>
2023-03-30 14:44:08 +0100

    implement event handler to add image to box selected

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('yo')
+const newGameBtn = document.querySelector('.button button');
+const gameGrid = document.querySelector('.grid');
+
+newGameBtn.addEventListener('click', startNewGame);
+
+gameGrid.addEventListener('click', function(e) {
+  const box = e.target
+  const image = document.createElement('img')
+  image.src = "images/x1.svg"
+  box.appendChild(image)
+});
+
+
+function startNewGame() {
+  //
+}
+
